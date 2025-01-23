c9c142d3eea7442e7a3f67b0e44808e2efae944b
hhkim9 <hannahkimclass-docker>
2025-01-23 04:22:12 +0000

    Add script to write current date and time to version.md

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from datetime import datetime
 
 now = datetime.now()
+
 current_time = now.strftime("%Y-%m-%d %H:%M:%S")
 
-with open("../version.md", "w") as file: 
-    file.write(f"Last updated: {current_time}")
+with open("../version.md", "w") as file:
+    file.write(f"Last updated: {current_time}\n")
+
+print(f"Current time written to version.md: {current_time}")
